fa675604aa841e995b59693f79fec7b4ab2b9832
PengYu1990 <yp1990@gmail.com>
2023-10-14 22:05:04 +0100

    Implement find current company and jobs of current company

--- src/main/java/com/pengyu/magnet/repository/JobRepository.java
+++ src/main/java/com/pengyu/magnet/repository/JobRepository.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
 
+    // Only query job with status ACTIVE and EXPIRED
     @Query("select j from Job j JOIN FETCH j.company where j.company =:company and (j.status=0 or j.status=1)")
     Page<Job> findAllByCompany(Pageable pageable, @Param("company") Company company);
 
